0391c7c13eb1ccb135d768db39778685c23cf14f
nidhi-ks <167076670+nidhi-ks@users.noreply.github.com>
2025-07-24 22:49:41 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -5,7 +5,7 @@ output "cc_compute_pool_name" {
 
 output "cc_hands_env" {
   description = "Confluent Cloud Environment ID"
-  value       = resource.confluent_environment.cc_handson_env.id
+  value       = confluent_environment.cc_handson_env.id 
 }
 
 output "cc_handson_sr" {
@@ -15,22 +15,44 @@ output "cc_handson_sr" {
 
 output "cc_sr_cluster" {
   description = "CC SR Cluster ID"
-  value       = resource.confluent_schema_registry_cluster.cc_sr_cluster.id
+  value       = confluent_schema_registry_cluster.cc_sr_cluster.id 
 }
 
 output "cc_kafka_cluster" {
   description = "CC Kafka Cluster ID"
-  value       = resource.confluent_kafka_cluster.cc_kafka_cluster.id
+  value       = confluent_kafka_cluster.cc_kafka_cluster.id 
 }
 
-output "datagen_products" {
-  description = "CC Datagen Products Connector ID"
-  value       = resource.confluent_connector.datagen_products.id
+# Updated connector outputs
+output "datagen_users" {
+  description = "CC Datagen Users Connector ID"
+  value       = confluent_connector.datagen_users.id
 }
 
-output "datagen_customers" {
-  description = "CC Datagen Customers Connector ID"
-  value       = resource.confluent_connector.datagen_customers.id
+output "datagen_trades_data" {
+  description = "CC Datagen Trades Data Connector ID"
+  value       = confluent_connector.datagen_trades_data.id
+}
+
+output "datagen_transaction_data" {
+  description = "CC Datagen Transaction Data Connector ID"
+  value       = confluent_connector.datagen_transaction_data.id
+}
+
+# New topic outputs (optional, but good for visibility)
+output "users_data_topic_name" {
+  description = "Users Data Topic Name"
+  value       = confluent_kafka_topic.users_data.topic_name
+}
+
+output "trades_data_topic_name" {
+  description = "Trades Data Topic Name"
+  value       = confluent_kafka_topic.trades_data.topic_name
+}
+
+output "transaction_data_topic_name" {
+  description = "Transaction Data Topic Name"
+  value       = confluent_kafka_topic.transaction_data.topic_name
 }
 
 
